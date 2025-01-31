d35e9e1728382cdad017559f3c60bfd242e538eb
Maxime Duhamel <110108406+Phenixis@users.noreply.github.com>
2025-01-31 11:45:09 +0100

    Client modifs (#7)
    * Update .gitignore to exclude client.out and tchatator.log files
    
    * Implement user authentication and session management
    
    * Refactor client menu options and improve role-based command handling
    
    * remove old menu

--- client.c
+++ client.c
@@ -4,56 +4,85 @@
 #include <unistd.h>
 #include <arpa/inet.h>
 
-void nettoyer_buffer() {
-    while (getchar() != '\n'); // Consomme les caractères jusqu'au '\n'
+void nettoyer_buffer()
+{
+    while (getchar() != '\n')
+        ; // Consomme les caractères jusqu'au '\n'
 }
 
-void afficher_menu(char *role) {
-    printf("\n=== Menu ===\n");
-    printf("1.  Me connecter\n");
-    printf("2.  Envoyer un message\n");
-    printf("3.  Me déconnecter et quitter\n");
-
-    // Si connecté
-    if (strcmp(role, "membre") == 0 || strcmp(role, "pro") == 0) {
-        printf("4.  Messages non lus\n");
-        printf("5.  Informations concernant un de mes messages\n");
-        printf("6.  Modifier un de mes messages\n");
-        printf("7.  Supprimer un de mes messages\n");
+void afficher_menu(char *role)
+{
+    // Si membre
+    if (strcmp(role, "membre") == 0)
+    {
+        printf("\n=== Menu ===\n");
+        printf("1. Envoyer un message\n");
+        printf("2. Messages non lus\n");
+        printf("3. Informations concernant un de mes messages\n");
+        printf("4. Modifier un de mes messages\n");
+        printf("5. Supprimer un de mes messages\n");
+        printf("6. Historique des messages avec un client\n");
+        printf("7. Me déconnecter et quitter\n");
     }
-
-    // Si pro
-    if (strcmp(role, "pro") == 0) {
-        printf("8.  Historique des messages avec un client\n");
-        printf("9.  Bloquer un client 24h\n");
-        printf("10  Lever le blocage d'un client 24h\n");
-        printf("11. Bannir un client définitivement\n");
-        printf("12. Lever le ban d'un client\n");
+    else if (strcmp(role, "pro") == 0)
+    {
+        printf("\n=== Menu ===\n");
+        printf("1. Envoyer un message\n");
+        printf("2. Messages non lus\n");
+        printf("3. Informations concernant un de mes messages\n");
+        printf("4. Modifier un de mes messages\n");
+        printf("5. Supprimer un de mes messages\n");
+        printf("6. Historique des messages avec un client\n");
+        printf("7. Bloquer un client 24h\n");
+        printf("8. Lever le blocage d'un client\n");
+        printf("9. Me déconnecter et quitter\n");
+    }
+    else if (strcmp(role, "admin") == 0)
+    {
+        printf("\n=== Menu ===\n");
+        printf("1. Bloquer un client 24h\n");
+        printf("2. Lever le blocage d'un client\n");
+        printf("3. Bannir un client définitivement\n");
+        printf("4. Lever le ban d'un client\n");
+        printf("5. Synchroniser les paramètres\n");
+        printf("6. Afficher les logs\n");
+        printf("7. Me déconnecter et quitter\n");
+    }
+    else
+    {
+        printf("\n=== Menu ===\n");
+        printf("1. Me connecter\n");
+        printf("2. Quitter\n");
     }
 
     printf("Choisissez une option: ");
 }
 
 // Mettre à jour le rôle du client (pro / membre / admin)
-void update_role(int sock, char *role) {
+void update_role(int sock, char *role)
+{
     send(sock, "/role", 6, 0);
 
     // Recevoir jusqu'à 19 caractères (en réservant 1 octet pour '\0')
     int bytes_received = recv(sock, role, 19, 0);
-    if (bytes_received >= 0) {
-        role[bytes_received] = '\0';  // Assurez-vous que la chaîne est correctement terminée
-    } else {
+    if (bytes_received >= 0)
+    {
+        role[bytes_received] = '\0'; // Assurez-vous que la chaîne est correctement terminée
+    }
+    else
+    {
         // Gérer les erreurs de réception ici (par exemple, socket déconnectée)
         perror("Erreur lors de la réception du rôle");
     }
 }
 
-void connexion(int sock) {
-    char *api_key = malloc(100 * sizeof(char));  // Allocation de mémoire pour api_key
+void connexion(int sock)
+{
+    char *api_key = malloc(100 * sizeof(char)); // Allocation de mémoire pour api_key
 
     printf("Entrez votre clé API: ");
     scanf("%s", api_key);
-    getchar();  // Pour consommer le newline laissé par scanf
+    getchar(); // Pour consommer le newline laissé par scanf
 
     // Construire la requête /connexion
     char requete[1024];
@@ -68,10 +97,12 @@ void connexion(int sock) {
     recv(sock, buffer, sizeof(buffer), 0);
     printf("Réponse du serveur: %s", buffer);
 
-    free(api_key);  // N'oubliez pas de libérer la mémoire allouée pour api_key
+    free(api_key); // N'oubliez pas de libérer la mémoire allouée pour api_key
 }
 
-void deconnexion(int sock) {
+void deconnexion(int sock)
+{
+    printf("Merci d'avoir utilisé Tchatator...\n");
     char *requete = "/deconnexion";
     send(sock, requete, strlen(requete), 0);
 
@@ -79,17 +110,18 @@ void deconnexion(int sock) {
     exit(0);
 }
 
-void envoyer_message(int sock) {
+void envoyer_message(int sock)
+{
     int id_compte;
     char message[1024];
 
     printf("Entrez l'ID du compte: ");
     scanf("%d", &id_compte);
-    getchar();  // Pour consommer le newline laissé par scanf
+    getchar(); // Pour consommer le newline laissé par scanf
 
     printf("Entrez le message à envoyer: ");
     fgets(message, sizeof(message), stdin);
-    message[strcspn(message, "\n")] = 0;  // Supprimer le newline à la fin du message
+    message[strcspn(message, "\n")] = 0; // Supprimer le newline à la fin du message
 
     // Construire la requête /message
     char requete[1224];
@@ -105,11 +137,11 @@ void envoyer_message(int sock) {
     printf("Réponse du serveur: %s", buffer);
 }
 
-
 // ######################
 // FONCTIONS A TERMINER #
 // ######################
-void messages_non_lus(int sock) {
+void messages_non_lus(int sock)
+{
     char *requete = "/liste";
     send(sock, requete, strlen(requete), 0);
 
@@ -117,7 +149,8 @@ void messages_non_lus(int sock) {
     exit(0);
 }
 
-void info_message(int sock) {
+void info_message(int sock)
+{
     char *requete = "/liste";
     send(sock, requete, strlen(requete), 0);
 
@@ -125,7 +158,8 @@ void info_message(int sock) {
     exit(0);
 }
 
-void modifier_message(int sock) {
+void modifier_message(int sock)
+{
     char *requete = "/liste";
     send(sock, requete, strlen(requete), 0);
 
@@ -133,7 +167,8 @@ void modifier_message(int sock) {
     exit(0);
 }
 
-void supprimer_message(int sock) {
+void supprimer_message(int sock)
+{
     char *requete = "/liste";
     send(sock, requete, strlen(requete), 0);
 
@@ -141,7 +176,8 @@ void supprimer_message(int sock) {
     exit(0);
 }
 
-void historique_message(int sock) {
+void historique_message(int sock)
+{
     char *requete = "/liste";
     send(sock, requete, strlen(requete), 0);
 
@@ -149,7 +185,8 @@ void historique_message(int sock) {
     exit(0);
 }
 
-void bloquer_client(int sock) {
+void bloquer_client(int sock)
+{
     char *requete = "/liste";
     send(sock, requete, strlen(requete), 0);
 
@@ -157,7 +194,8 @@ void bloquer_client(int sock) {
     exit(0);
 }
 
-void enlever_blocage(int sock) {
+void enlever_blocage(int sock)
+{
     char *requete = "/liste";
     send(sock, requete, strlen(requete), 0);
 
@@ -165,7 +203,8 @@ void enlever_blocage(int sock) {
     exit(0);
 }
 
-void bannir_client(int sock) {
+void bannir_client(int sock)
+{
     char *requete = "/liste";
     send(sock, requete, strlen(requete), 0);
 
@@ -173,7 +212,8 @@ void bannir_client(int sock) {
     exit(0);
 }
 
-void enlever_ban(int sock) {
+void enlever_ban(int sock)
+{
     char *requete = "/liste";
     send(sock, requete, strlen(requete), 0);
 
@@ -185,120 +225,172 @@ void enlever_ban(int sock) {
 // #######################
 
 // Traitement de la commande de l'utilisateur (avec vérification de rôle)
-void traiter_commande(int choix, int sock, char *role) {
-    switch (choix) {
-        case 1:
-            connexion(sock);
-            update_role(sock, role);  // Met à jour le rôle après la connexion
-            break;
-        case 2:
+void traiter_commande(int choix, int sock, char *role)
+{
+    switch (choix)
+    {
+    case 1:
+        if (strcmp(role, "membre") == 0 | strcmp(role, "pro") == 0)
+        {
             envoyer_message(sock);
-            break;
-        case 3:
-            printf("Merci d'avoir utilisé Tchatator...\n");
+        }
+        else if (strcmp(role, "admin") == 0)
+        {
+            bloquer_client(sock);
+        }
+        else
+        {
+            connexion(sock);
+            update_role(sock, role); // Met à jour le rôle après la connexion
+        }
+        break;
+
+    case 2:
+        if (strcmp(role, "membre") || strcmp(role, "pro"))
+        {
+            messages_non_lus(sock);
+        }
+        else if (strcmp(role, "admin"))
+        {
+            enlever_blocage(sock);
+        }
+        else
+        {
             deconnexion(sock);
-            break;
+        }
+        break;
 
-        // Options disponibles selon le rôle
-        case 4:
-            if (strcmp(role, "membre") || strcmp(role, "pro")) {
-                messages_non_lus(sock);
-            } else {
-                printf("Option non disponible pour votre rôle.\n");
-            }
-            break;
+    case 3:
+        if (strcmp(role, "membre") || strcmp(role, "pro"))
+        {
+            info_message(sock);
+        }
+        else if (strcmp(role, "admin"))
+        {
+            bannir_client(sock);
+        }
+        else
+        {
+            printf("Option non disponible pour votre rôle.\n");
+        }
+        break;
 
-        case 5:
-            if (strcmp(role, "membre") || strcmp(role, "pro")) {
-                info_message(sock);
-            } else {
-                printf("Option non disponible pour votre rôle.\n");
-            }
-            break;
+    case 4:
+        if (strcmp(role, "membre") || strcmp(role, "pro"))
+        {
+            modifier_message(sock);
+            // messages_non_lus(sock);
+        }
+        else if (strcmp(role, "admin"))
+        {
+            enlever_ban(sock);
+        }
+        else
+        {
+            printf("Option non disponible pour votre rôle.\n");
+        }
+        break;
 
-        case 6:
-            if (strcmp(role, "membre") || strcmp(role, "pro")) {
-                modifier_message(sock);
-            } else {
-                printf("Option non disponible pour votre rôle.\n");
-            }
-            break;
+    case 5:
+        if (strcmp(role, "membre") || strcmp(role, "pro"))
+        {
+            supprimer_message(sock);
+            // info_message(sock);
+        }
+        else if (strcmp(role, "admin"))
+        {
+            sync(sock);
+        }
+        else
+        {
+            printf("Option non disponible pour votre rôle.\n");
+        }
+        break;
 
-        case 7:
-            if (strcmp(role, "membre") || strcmp(role, "pro")) {
-                supprimer_message(sock);
-            } else {
-                printf("Option non disponible pour votre rôle.\n");
-            }
-            break;
+    case 6:
+        if (strcmp(role, "membre") || strcmp(role, "pro"))
+        {
+            historique_message(sock);
+            // modifier_message(sock);
+        }
+        else if (strcmp(role, "admin"))
+        {
+            logs(sock);
+        }
+        else
+        {
+            printf("Option non disponible pour votre rôle.\n");
+        }
+        break;
 
-        case 8:
-            if (strcmp(role, "pro")) {
-                historique_message(sock);
-            } else {
-                printf("Option non disponible pour votre rôle.\n");
-            }
-            break;
+    case 7:
+        if (strcmp(role, "membre") || strcmp(role, "admin"))
+        {
+            deconnexion(sock);
+            // supprimer_message(sock);
+        }
+        else if (strcmp(role, "pro"))
+        {
+            bloquer_client(sock);
+        }
+        else
+        {
+            printf("Option non disponible pour votre rôle.\n");
+        }
+        break;
 
-        case 9:
-            if (strcmp(role, "pro")) {
-                bloquer_client(sock);
-            } else {
-                printf("Option non disponible pour votre rôle.\n");
-            }
-            break;
+    case 8:
+        if (strcmp(role, "pro"))
+        {
+            enlever_blocage(sock);
+            // historique_message(sock);
+        }
+        else
+        {
+            printf("Option non disponible pour votre rôle.\n");
+        }
+        break;
 
-        case 10:
-            if (strcmp(role, "pro")) {
-                enlever_blocage(sock);
-            } else {
-                printf("Option non disponible pour votre rôle.\n");
-            }
-            break;
+    case 9:
+        if (strcmp(role, "pro"))
+        {
+            deconnexion(sock);
+        }
+        else
+        {
+            printf("Option non disponible pour votre rôle.\n");
+        }
+        break;
 
-        case 11:
-            if (strcmp(role, "pro")) {
-                bannir_client(sock);
-            } else {
-                printf("Option non disponible pour votre rôle.\n");
-            }
-            break;
-
-        case 12:
-            if (strcmp(role, "pro")) {
-                enlever_ban(sock);
-            } else {
-                printf("Option non disponible pour votre rôle.\n");
-            }
-            break;
-
-        default:
-            printf("Option invalide, essayez encore.\n");
+    default:
+        printf("Option invalide, essayez encore.\n");
     }
 }
 
-
-
-
-
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
     // Vérifier si un port est passé en paramètre
-    if (argc <= 1) {
-        perror("Aucun port spécifié");
-        exit(1);
+    int port;
+    if (argc <= 1)
+    {
+        port = 8080;
     }
-
-    int port = atoi(argv[1]);
-    if (!port) {
-        perror("Veuilleez saisir un port valide en paramètre");
-        exit(1);
+    else
+    {
+        port = atoi(argv[1]);
+        if (!port)
+        {
+            perror("Veuillez saisir un port valide en paramètre");
+            exit(1);
+        }
     }
 
     int sock;
     struct sockaddr_in server_addr;
     // Créer le socket
     sock = socket(AF_INET, SOCK_STREAM, 0); // Connexion en protocole TCP
-    if (sock == -1) {
+    if (sock == -1)
+    {
         perror("Création de la socket échouée");
         exit(1);
     }
@@ -308,7 +400,8 @@ int main(int argc, char *argv[]) {
     server_addr.sin_addr.s_addr = inet_addr("127.0.0.1"); // Connexion au serveur local
 
     // Tentative de connexion avec le serveur
-    if (connect(sock, (struct sockaddr *)&server_addr, sizeof(server_addr)) == -1) {
+    if (connect(sock, (struct sockaddr *)&server_addr, sizeof(server_addr)) == -1)
+    {
         perror("Impossible de se connecter au Tchatator");
         exit(1);
     }
@@ -317,18 +410,20 @@ int main(int argc, char *argv[]) {
     char role[20] = "aucun";
 
     // Boucle principale pour afficher le menu et traiter les options
-    while (1) {
+    while (1)
+    {
         afficher_menu(role);
-        
+
         // Lire l'option avec fgets pour éviter les caractères invalides
         char buffer[10];
         fgets(buffer, sizeof(buffer), stdin);
 
         // Vérifier si l'entrée est un nombre valide
-        if (sscanf(buffer, "%d", &choix) != 1) {
+        if (sscanf(buffer, "%d", &choix) != 1)
+        {
             printf("Entrée invalide, veuillez saisir un nombre.\n");
-            nettoyer_buffer();  // Nettoie le tampon pour éviter les caractères restants
-            continue;  // Redemander une entrée
+            nettoyer_buffer(); // Nettoie le tampon pour éviter les caractères restants
+            continue;          // Redemander une entrée
         }
 
         traiter_commande(choix, sock, role);
